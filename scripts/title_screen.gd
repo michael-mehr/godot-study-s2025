a6a933b2da68c66ddccf2d44a4860021ed1a0e83
Michael Mehr <michaelmehr0@gmail.com>
2025-03-18 14:04:48 -0500

    title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -5,3 +5,7 @@ var main_scene = preload("res://scenes/main.tscn")
 func _on_start_button_pressed():
   get_tree().change_scene_to_packed(main_scene)
   pass
+
+func _on_quit_button_pressed():
+  get_tree().quit()
+  pass # Replace with function body.
