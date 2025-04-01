f6ca26e114345aa7147b350e49e24bad57e11575
Michael Mehr <michaelmehr0@gmail.com>
2025-04-01 17:55:27 -0500

    removed debug code

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,6 @@
 extends Area3D
 
 func _on_body_entered(body):
-	print("body entered coin")
 	if body.has_method("collect_coin"):
-		print("body has collect_coin")
 		body.collect_coin()
 		queue_free()
