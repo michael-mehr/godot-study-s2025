2e65d0f44891ff20a12056fa85864f86be01c808
Michael Mehr <michaelmehr0@gmail.com>
2025-05-02 15:22:34 -0500

    updated view.gd

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -8,4 +8,3 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
   get_tree().quit()
-  pass # Replace with function body.
--- scripts/view.gd
+++ scripts/view.gd
@@ -3,25 +3,22 @@ extends Node3D
 @export_group("Properties")
 @export var target: Node
 
-@export_group("Rotation")
-@export var rotation_speed : int = 120
+# @export_group("Rotation")
+# @export var rotation_speed : int = 120
 
-var camera_rotation : Vector3
-var zoom = 10
+# var camera_rotation : Vector3
+# var zoom = 10
 
 @onready var camera : Camera3D = $Camera
 
-func _ready():
-	
-	camera_rotation = rotation_degrees # Initial rotation
-	
-	pass
+# func _ready():
+# 	camera_rotation = rotation_degrees # Initial rotation
 
 func _physics_process(delta):
 	
-	# Set position and rotation to targets
-	
+	# Set position to player target
 	self.position = self.position.lerp(target.position, delta * 4)
-	rotation_degrees = rotation_degrees.lerp(camera_rotation, delta * 6)
 	
+	# For the camera to turn with the player:
+	# rotation_degrees = rotation_degrees.lerp(camera_rotation, delta * 6)
 	# camera.position = camera.position.lerp(Vector3(0, 0, zoom), 8 * delta)
